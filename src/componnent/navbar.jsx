50c8b102a6f70a588a427a8b3df537dff91b615b
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-19 08:42:59 +0700

    fix bug path

--- src/componnent/navbar.jsx
+++ src/componnent/navbar.jsx
@@ -1,10 +1,11 @@
 import { Info } from "react-feather";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
       <div className="w-full h-16 flex justify-items-center items-center justify-between">
         <h1 className="font-inter cursor-pointer font-bold text-sky-500 ml-2 hover:text-sky-400"><a href="/">MochiTrecker</a></h1>
-        <a className="cursor-pointer text-sm mr-5" href="/info"><Info size="20px" color="oklch(68.5% 0.169 237.323)" /></a>
+        <a className="cursor-pointer text-sm mr-5"><Link to="/info"><Info size="20px" color="oklch(68.5% 0.169 237.323)" /></Link></a>
       </div>
     </>
   )
