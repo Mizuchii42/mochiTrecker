4334fbc106198c0ad55c37e65ffab74d27ba0760
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-20 00:59:46 +0700

    add ui homepage & fix bug navbar bottom

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Navbar from './componnent/navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import InfoSevice from './page/info'
 import Navbutton from './componnent/navButton'
+import { Home } from 'react-feather'
+import Homepage from './page/home'
 function App() {
 
   return (
     <>
-      <div className='container mx-auto h-svh w-full md:w-[45%]'>
+      <div className=' h-svh w-full'>
         <Router>
           <Navbar />
           <Routes>
+            <Route path='/' element={<Homepage />} />
             <Route path='/info' element={<InfoSevice />} />
           </Routes>
           <Navbutton />
---
+++ src/page/home.jsx
@@ -0,0 +1,28 @@
+import { TrendingDown, TrendingUp } from "react-feather";
+const Homepage = () => {
+  return (
+    <>
+      <div className="w-full h-svh">
+        <div className="w-[98%] h-40 rounded-md opacity-50 mx-auto flex justify-center justify-items-center items-center">
+          <div className="text-center">
+            <small className="font-inter text-gray-500">total dana</small>
+            <h1 className="font-inter font-extrabold text-black text-3xl">30.0000</h1>
+          </div>
+        </div>
+
+        <div className="w-full h-16 flex justify-items-center items-center justify-center gap-1">
+          <div className="w-[45%] h-full flex justify-center justify-items-center items-center gap-2 font-inter text-green-600">
+            <TrendingUp color="green" /> 0.0
+          </div>
+          <div className="w-[45%] h-full  flex justify-center justify-items-center items-center gap-2 font-inter text-red-600">
+            <TrendingDown color="red" /> 0.0
+          </div>
+
+        </div>
+        <h1 className="font-bold ml-2 text-1xl text-sky-500">Riwayat digital</h1>
+      </div>
+    </>
+  )
+}
+
+export default Homepage;
