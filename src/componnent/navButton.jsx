352c0cc0aa4d0b70d904c7b3b7909fa1a513998f
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-20 00:10:57 +0700

    fix navigasi bar buttom

--- src/componnent/navButton.jsx
+++ src/componnent/navButton.jsx
@@ -3,7 +3,7 @@ import { Home, BookOpen, PlusCircle, Settings, HelpCircle } from "react-feather"
 const Navbutton = () => {
   return (
     <>
-      <div className="w-full h-16 flex justify-items-center items-center fixed bottom-0 gap-12 ml-2">
+      <div className="w-full h-16 flex justify-items-center items-center fixed bottom-0 gap-11">
         <Link to="/"><Home color="oklch(68.5% 0.169 237.323)" size="25px" /></Link>
         <Link to="/history"><BookOpen color="oklch(68.5% 0.169 237.323)" size="25px" /></Link>
         <Link to="/add"><PlusCircle color="oklch(68.5% 0.169 237.323)" size="25px" /></Link>
