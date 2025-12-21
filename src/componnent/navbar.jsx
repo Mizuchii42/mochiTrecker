2c9fddb07c1d1a0d087f4cbf835502972f1d1690
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-21 11:15:12 +0700

    banting setir

--- src/componnent/navbar.jsx
+++ src/componnent/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <div className="w-full h-16 flex justify-items-center items-center justify-between">
-        <h1 className="font-inter cursor-pointer font-bold text-sky-500 ml-2 hover:text-sky-400"><a href="/">MochiTrecker</a></h1>
+        <h1 className="font-inter cursor-pointer font-bold text-sky-500 ml-2 hover:text-sky-400"><a href="/">ToramSensei</a></h1>
         <a className="cursor-pointer text-sm mr-5"><Link to="/info"><Info size="20px" color="oklch(68.5% 0.169 237.323)" /></Link></a>
       </div>
     </>
--- src/page/info.jsx
+++ src/page/info.jsx
@@ -4,8 +4,8 @@ const InfoSevice = () => {
       <div className="font-inter">
         <div className="md:w-full mx-auto w-[97%] h-24 rounded-sm border border-sky-500 flex justify-items-center items-center">
           <div>
-            <h1 className="text-sky-500 font-bold ml-3">mochiTrecker</h1>
-            <p className="text-sm ml-3">mochi merupakan sebuah website menejemen keuangan yang praktis dan dapat di gunakan di mana saja</p>
+            <h1 className="text-sky-500 font-bold ml-3">ToramSensei</h1>
+            <p className="text-sm ml-3">ToramSensei adalah aplikasi serba guna bagi para pemain toram online menyediakan berbagai kebutuhan informasi dalam game</p>
           </div>
         </div>
       </div>
